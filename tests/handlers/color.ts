c20d6e5a67759b7da7143e22f9c87cd867524e73
Christopher Wallis <supercryptonite@gmail.com>
2017-01-26 00:30:06 -0500

    add tests for parse color

--- tests/handlers/color.ts
+++ tests/handlers/color.ts
@@ -2,9 +2,34 @@ import { parseColor } from '../../src/handlers';
 import * as assert from 'assert';
 const jsdom = require('mocha-jsdom');
 
-describe('color', () => {
-    jsdom();
-    it('parses #0000ff as blue', () => {
-        assert.deepEqual([0, 0, 255, 1], parseColor('#0000ff'));
-    });
+describe('parseColor', () => {
+  jsdom();
+
+  it('parses #FFF as white', () => {
+    assert.deepEqual([255, 255, 255, 1], parseColor('#FFF'));
+  });
+
+  it('parses #0000ff as blue', () => {
+    assert.deepEqual([0, 0, 255, 1], parseColor('#0000ff'));
+  });
+
+  it('parses "red" as red', () => {
+    assert.deepEqual([255, 0, 0, 1], parseColor('red'));
+  });
+
+  it('parses rgb(0, 255, 0) as green', () => {
+    assert.deepEqual([0, 255, 0, 1], parseColor('rgb(0,255,0)'));
+  });
+
+  it('parses rgba(0, 255, 0, 1) as green', () => {
+    assert.deepEqual([0, 255, 0, 1], parseColor('rgba(0,255,0,1)'));
+  });
+
+  it('parses hsl(0, 100%, 50%) as red', () => {
+    assert.deepEqual([255, 0, 0, 1], parseColor('hsl(0,100%,50%)'));
+  });
+
+  it('parses hsla(0, 100%, 50%, 1) as red', () => {
+    assert.deepEqual([255, 0, 0, 1], parseColor('hsl(0,100%,50%,1)'));
+  });
 });
